1b5b7d7a5bc62cb4aaba13b24c5c2ac003991c8e
saahilsheikh5-cell <saahilsheikh5@gmail.com>
2025-08-23 18:25:01 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from telebot import types
 
 # === CONFIG (hardcoded) ===
 BOT_TOKEN = os.environ.get("7638935379:AAEmLD7JHLZ36Ywh5tvmlP1F8xzrcNrym_Q")
-WEBHOOK_URL = "https://shayobot.onrender.com/" + BOT_TOKEN
+WEBHOOK_URL = "https://shayobott.onrender.com/" + BOT_TOKEN
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # Default portfolio and watchlist
